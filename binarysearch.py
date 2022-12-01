9ea32442cb46c0fdebe845b01c640f4c084d2a5a
ravi25100106 <67362175+ravi25100106@users.noreply.github.com>
2022-12-01 09:28:11 +0530

    binary search in python

--- binarysearch.py
+++ binarysearch.py
@@ -1,18 +1,21 @@
+# iterate using while loop until key is found
 def result(l,low,high,key):
-    while low<=high:
+    while low<=high:     
         mid=(low+high)//2
         if l[mid]==key:
             return mid
-        elif l[mid]>key:
+        elif l[mid]>key:    #if key is smaller select the lower sub-list
             high=mid-1
-        elif l[mid]<key:
+        elif l[mid]<key:    #if key is bigger select the higher sub-list
             low=mid+1
     return -1
+
+#given list
 l=[5,8,9,10,32,36,37,45]
-low=0
-high=len(l)
-key=int(input("enter key:"))
-res=result(l,low,high,key)
+low=0  #lower index
+high=len(l)  #higher index
+key=int(input("enter key:"))   #search element
+res=result(l,low,high,key)   #menthod calling
 if res==-1:
     print("key not found for first list")
 else:
